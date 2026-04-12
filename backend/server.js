84245667e2d470e0036114e5bd1db9ae26364d75
Felix <felabayomi@gmail.com>
2026-04-12 17:05:31 -0400

    Allow tours.citydiscoverer.guide in backend CORS origins

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@ const defaultAllowedOrigins = [
     'https://expeditionamerica.online',
     'https://www.expeditionamerica.online',
     'https://citytourhub.vercel.app',
+    'https://tours.citydiscoverer.guide',
 ];
 const allowedOrigins = [...new Set([...defaultAllowedOrigins, ...envAllowedOrigins])];
 const isLocalDevOrigin = (origin = '') => /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/i.test(origin);
