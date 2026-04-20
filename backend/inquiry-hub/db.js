e231d9ed327da73d2ed3977bb3407e144894c4ca
Felix <felabayomi@gmail.com>
2026-04-20 19:55:08 -0400

    inquiry-hub: always use SSL for db pool

--- backend/inquiry-hub/db.js
+++ backend/inquiry-hub/db.js
@@ -4,11 +4,9 @@ const { drizzle } = require('drizzle-orm/node-postgres');
 const schema = require('./schema');
 
 const connectionString = process.env.INQUIRY_HUB_DATABASE_URL || process.env.DATABASE_URL;
-const isProduction = process.env.NODE_ENV === 'production';
-
 const pool = new Pool({
-  connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : undefined,
+    connectionString,
+    ssl: { rejectUnauthorized: false },
 });
 
 const db = drizzle(pool, { schema });
